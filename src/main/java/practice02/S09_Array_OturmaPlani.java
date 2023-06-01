5ecd38d45a9c08f12fb215032fbabb1f3ae0c861
Gürkay Birinci <117175953+gurkaybirinci@users.noreply.github.com>
2023-06-01 14:33:18 +0300

    01.06.23

--- src/main/java/practice02/S09_Array_OturmaPlani.java
+++ src/main/java/practice02/S09_Array_OturmaPlani.java
@@ -21,20 +21,3 @@ public class S09_Array_OturmaPlani {
         System.out.println(Arrays.toString(oturmaPlani));
     }
 }
-    /*
-        Kelime Tahmin Oyunu
-        Kullanıcının bir kelimeyi harf harf tahmin ederek doğru kelimeyi bulmasını sağlayan bir oyun geliştirin.
-        Oyun şu adımları içermelidir:
-
-        - Bir kelime listesi tanımlayın. Bu listede en az 5 farklı kelime bulunsun.
-        - Oyuncu, listeden rastgele seçilen bir kelimeyi tahmin etmeye çalışmalıdır.
-        - Oyuncuya, seçilen kelimenin uzunluğu kadar alt tire "_" işaretinden  oluşan bir dizi gösterilmelidir.
-          Örneğin, seçilen kelime "elma" ise "____" şeklinde bir gösterim yapılmalıdır.
-        - Oyuncu, her tahmininde bir harf girmelidir.
-            - Girdiği harf, seçilen kelimede bulunuyorsa, doğru tahmin olduğu belirtilmeli ve
-              ilgili harf yerine konulmalıdır.
-            - Oyuncu, yanlış tahminlerde bulunduğunda, kalan hakkı azalmalıdır.
-              Başlangıçta oyuncunun 5 hakka sahip olduğu varsayılmalıdır.
-        - Oyuncunun doğru kelimeyi bulması durumunda, tebrik mesajı gösterilmeli ve oyun sonlandırılmalıdır.
-        - Oyuncunun kalan hakları sıfır olduğunda, olumsuz bir mesaj ve doğru kelime gösterilmelidir.
-     */
